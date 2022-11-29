cc0095e00e43a1f236a4868e93c118301b8f4f45
Christian Pala <pala.christian@gmail.com>
2022-11-29 19:46:52 +0100

    binary mask for the sky.

--- initial_dataset.py
+++ initial_dataset.py
@@ -1,9 +1,12 @@
 # start with a sample of 174 images from Aachen for our dataset:
 # We need to get the matching masks in the gtFine folder
 
-
+# Libraries:
 from glob import glob
 from pathlib import Path
+from typing import Optional
+import numpy as np
+import skimage
 
 
 def create_initial_dataset_folder_with_aachen_images() -> None:
@@ -18,7 +21,7 @@ def create_initial_dataset_folder_with_aachen_images() -> None:
             Path(image_path).rename(Path("initial_dataset") / Path(image_path).name)
 
 
-def get_image_segmentation(image_path, segmentation_path):
+def get_image_segmentation(image_path, segmentation_path) -> Optional[Path]:
     """Get the segmentation for the image.
 
     Args:
@@ -41,11 +44,30 @@ def rename_images_and_masks() -> None:
     Renames the images and masks to a more succinct format:
     """
     Path("initial_dataset").mkdir(exist_ok=True)
-    for i, image_path in enumerate(glob("initial_dataset/*.png")):
-        if i % 2 == 1:
-            Path(image_path).rename(Path("initial_dataset") / f"image_{i // 2}.png")
-        else:
-            Path(image=Path(image_path).rename(Path("initial_dataset") / f"mask_{i // 2}.png"))
+
+    # if the images are already correctly named, we don't need to rename them:
+    if len(glob("initial_dataset/image_0.png")) == 0:
+        for i, image_path in enumerate(glob("initial_dataset/*.png")):
+            if i % 2 == 1:
+                Path(image_path).rename(Path("initial_dataset") / f"image_{i // 2}.png")
+            else:
+                Path(image=Path(image_path).rename(Path("initial_dataset") / f"mask_{i // 2}.png"))
+
+
+def binary_mask(mask) -> np.ndarray:
+    """
+    Converts the mask to a binary mask, keeping only the sky class as 1 and the rest as 0
+    :param mask: the mask to convert
+    :return: the binary mask
+    """
+    # from the docs, sky has the following RGB values: 70,130,180.
+    # https://github.com/mcordts/cityscapesScripts/blob/master/cityscapesscripts/helpers/labels.py
+    # sky mask: 70,130,180
+    sky = np.all(mask == [70, 130, 180], axis=-1)
+    # set to 1 the sky pixels and 0 the rest
+    mask[sky] = [1., 1., 1.]
+    mask[~sky] = [0., 0., 0.]
+    return mask
 
 
 def main() -> None:
@@ -63,6 +85,11 @@ def main() -> None:
         Path(segmentation_path).rename(Path("initial_dataset") / Path(segmentation_path).name)
 
     rename_images_and_masks()
+
+    for mask_path in glob("initial_dataset/*.png"):
+        mask = skimage.io.imread(mask_path)
+        mask = binary_mask(mask)
+        skimage.io.imsave(mask_path, mask)
 
 
 if __name__ == "__main__":
