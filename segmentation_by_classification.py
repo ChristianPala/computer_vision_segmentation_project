b3e4370ba77df136f07aa14f58b43849cf4050a8
Manuel-Ippolito <manuelippolito01@gmail.com>
2022-11-29 20:02:59 +0100

    Added to segmentation_by_classification.py the function get_image_and_segmentation to get an image and its segmentation given their index number.

--- segmentation_by_classification.py
+++ segmentation_by_classification.py
@@ -1,15 +1,20 @@
 import os
+import numpy as np
 import matplotlib.pyplot as plt
 import skimage
+from typing import Union
 
 
-def visualize_image_and_segmentation(img_number: str) -> None:
+def get_image_and_segmentation(img_number: Union[str, int]) -> (np.ndarray, np.ndarray):
     """
-    Plots side-by-side the image corresponding to the given number and its full segmentation.
+    Gets the image corresponding to the given number and its full segmentation.
     Args:
         img_number: Index of the image desired to be plotted
-    Returns: None. It plots directly the corresponding images.
+    Returns: image and segmentation as numpy ndarrays.
     """
+    # Check that the given image number is a string
+    img_number = str(img_number)
+
     # Get the paths of the desired image and segmentation
     cwd_path = os.getcwd()
     img_path = cwd_path + '/initial_dataset/' + 'image_' + img_number + '.png'
@@ -18,6 +23,18 @@ def visualize_image_and_segmentation(img_number: str) -> None:
     # Get the actual image and segmentation as numpy ndarrays
     im = skimage.io.imread(img_path)
     segmentation = skimage.io.imread(segmentation_path)
+    return im, segmentation
+
+
+def visualize_image_and_segmentation(img_number: Union[str, int]) -> None:
+    """
+    Plots side-by-side the image corresponding to the given number and its full segmentation.
+    Args:
+        img_number: Index of the image desired to be plotted
+    Returns: None. It plots directly the corresponding images.
+    """
+    # Get the actual image and segmentation as numpy ndarrays
+    im, segmentation = get_image_and_segmentation(img_number)
 
     # Plot the image and its segmentation side-by-side
     fig, (ax0, ax1) = plt.subplots(nrows=1, ncols=2)
