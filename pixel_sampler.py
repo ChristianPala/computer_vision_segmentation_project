1d9a44dba962b2631bde5028db6d6883fd34a23a
Christian Pala <pala.christian@gmail.com>
2022-12-01 11:12:52 +0100

    Pixel sampler.

--- pixel_sampler.py
+++ pixel_sampler.py
@@ -5,6 +5,7 @@ import os
 import numpy as np
 import glob
 import skimage
+import pandas as pd
 
 # Global variables:
 from config import TRAINING_DATASET_PATH, TESTING_DATASET_PATH
@@ -81,5 +82,21 @@ def sample_pixels(total_count: int, train: bool = True) -> np.ndarray:
         sampled_pixels.append(non_sky_pixels)
 
     return np.array(sampled_pixels)
+
+
+def main() -> None:
+    """
+    Main function to sample 10000 pixels from the training dataset,
+    taking 5000 from the sky class and 5000 from the rest of the classes.
+    """
+    # sample 10000 pixels from the training dataset
+    sampled_pixels_tr = sample_pixels(10000, train=True)
+    train_df = pd.DataFrame(sampled_pixels_tr.reshape(-1, 3))
+    train_df.to_csv('train.csv', index=False)
+
+    # sample 5000 pixels from the testing dataset
+    sampled_pixels_te = sample_pixels(5000, train=False)
+    test_df = pd.DataFrame(sampled_pixels_te.reshape(-1, 3))
+    test_df.to_csv('test.csv', index=False)
 
 
